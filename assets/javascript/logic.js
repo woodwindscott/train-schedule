61922492a1a657f55a77d9b77983ec1763b141f3
Scott Moore <woodwindscott@yahoo.com>
2019-08-15 11:13:48 -0400

    Updates to train timetable display and refresh button functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -23,34 +23,91 @@ $(document).ready(function() {
     var nextTrain = "";
     var minutesTillTrain = "";
 
+    // This function will render the display of the train timetable - whenever the page is loaded or the refresh button is clicked.
+    function renderTrains() {
 
-    function trainCalculations (firstTrain, trainFrequency) {
+      // Firebase watcher .on("child_added")
+      database.ref().on("child_added", function(snapshot) {
+        
+          // storing the snapshot.val() in a variable for convenience
+          var sv = snapshot.val();
 
-        // First Time (pushed back 1 year to make sure it comes before current time)
-        var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-        console.log(firstTimeConverted);
-    
-        // Current Time
-        var currentTime = moment();
-        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm A"));
-    
-        // Difference between the times
-        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
-    
-        // Time apart (remainder)
-        var tRemainder = diffTime % trainFrequency;
-        console.log(tRemainder);
-    
-        // Minute Until Train
-        minutesTillTrain = trainFrequency - tRemainder;
-        console.log(" MINUTES TILL TRAIN: " + minutesTillTrain);
-    
-        // Next Train
-        nextTrain = moment().add(minutesTillTrain, "minutes");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm A"));
-    
+          trainCalculations (sv.firstTrainTime, sv.frequency, sv.dateAdded);
+
+          var lastUpdated = moment();
+          // console.log(moment(lastUpdated).format("MMMM Do YYYY, h:mm:ss a"));
+          $("#last-update").text(moment(lastUpdated).format("MMMM Do YYYY, h:mm a"));
+
+          var displayRow = $("<tr>");
+          var nameTD = $("<td>");
+          nameTD.text(sv.name);
+
+          var destinationTD = $("<td>")
+          destinationTD.text(sv.destination);
+
+          var frequencyTD = $("<td>");
+          frequencyTD.text(sv.frequency);
+
+          var arrivalTD = $("<td>");
+          arrivalTD.text(nextTrain);
+
+          var minutesTD = $("<td>");
+          minutesTD.text(minutesTillTrain);
+
+          displayRow.append(nameTD, destinationTD, frequencyTD, arrivalTD, minutesTD);
+          $("tbody").append(displayRow);
+
+        // Handle the errors
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
+
     }
+
+    function trainCalculations (firstTrain, trainFrequency, dateAdded) {
+
+        // Determines if the train was added today or on an earlier date - ie. is it already running?
+        var dayAdded = moment(dateAdded).format("MM/DD");
+        var today = moment().format("MM/DD");
+
+        var firstDeparture = moment(firstTrain, "HH:mm");
+        var isRunning = moment().diff(moment(firstDeparture), "minutes");
+
+        // If the train is already running (either added on a previous day or the train has started today), follow this script
+        if (dayAdded !== today || isRunning >= 0) {
+
+            // First Time (pushed back 1 year to make sure it comes before current time)
+            var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+    
+            // Difference between the times
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    
+            // Time apart (remainder)
+            var tRemainder = diffTime % trainFrequency;
+    
+            // Minute Until Train
+            minutesTillTrain = trainFrequency - tRemainder;
+    
+            // Next Train
+            nextTrain = moment().add(minutesTillTrain, "minutes").format("hh:mm A");
+
+        // If the train was added today and has not yet departed
+        } else {
+
+            // Minute Until first departure of the train (converted to a positive integer)
+            minutesTillTrain = isRunning * -1;
+            
+             // Next Train (Original Departure Time)
+             nextTrain = moment(firstDeparture).format("hh:mm A");
+
+        }
+  
+    }
+
+    // Upon the load of the page, this function will display the timetable
+
+    $("tbody").empty();
+    renderTrains();
 
     // Capture Button Click
     $("#submit-train").on("click", function(event) {
@@ -77,56 +134,21 @@ $(document).ready(function() {
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
 
+      //Whenever a new train is added, the timetable will be updated
+      $("tbody").empty();
+      renderTrains();
+
     });
 
-    // Firebase watcher .on("child_added"
-    database.ref().on("child_added", function(snapshot) {
-        // storing the snapshot.val() in a variable for convenience
-        var sv = snapshot.val();
+    // This will allow the user to refresh the train timetable
+    $("#refresh").on("click", function(event) {
+     
+      event.preventDefault();
 
-        trainCalculations (sv.firstTrainTime, sv.frequency)
+      // This will refresh the train timetable
+      $("tbody").empty();
+      renderTrains();
 
-        // Console.loging the last user's data
-        // console.log("Train Name: " + sv.name);
-        // console.log("Destination: " + sv.destination);
-        // console.log("First Train Time: " + sv.firstTrainTime);
-        // console.log("Frequency: " + sv.frequency);
-
-        // Change the HTML to reflect
-        // var nameTD = $("<td>");
-        // nameTD.text(sv.name);
-        // console.log(nameTD);
-
-        console.log(nextTrain);
-        console.log(minutesTillTrain);
-        var displayRow = "<tr><td>" + sv.name + "</td><td>" + sv.destination + "</td><td>" + sv.frequency + "</td><td>" + moment(nextTrain).format("hh:mm A") + "</td><td>" + minutesTillTrain + "</td></tr>";
-
-        // var destinationTD = $("<td>")
-        // destinationTD.text(sv.destination);
-        // var frequencyTD = $("<td>");
-        // frequencyTD.text(sv.frequency);
-        // var arrivalTD = $("<td>");
-        // arrivalTD.text("Placeholder 1");
-        // var minutesTD = $("<td>");
-        // minutesTD.text("Placeholder 2");
-
-        // var displayRow = $("<tr>");
-        // displayRow.append(nameTD + destinationTD + frequencyTD + arrivalTD + minutesTD);
-        $("tbody").append(displayRow);
-
-        // console.log(displayRow);
-        // console.log(nameTD);
-        console.log(sv.name);
-
-
-
-      // Handle the errors
-    }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
     });
-
-
-
-
 
 });
